ff209aceb023e0b50b7dcc100d27e83525370ec1
Freya-c23 <freya.claes@student.ehb.be>
2021-10-07 10:28:46 +0200

    Fetched names

--- Session 2/js/index.js
+++ Session 2/js/index.js
@@ -6,6 +6,19 @@ let pokemonlist = {
         content.innerHTML = " ";
         fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`, {})
             .then(response => response.json())
-            .then(data => {});
+            .then(data => {
+                data.results.forEach(function (pokemon) {
+                    const pokList = `
+                    <div class="pokemon">
+                        <img src="https://picsum.photos/200" alt="">
+                        <h1 id="name">${pokemon.name}</h1>
+                        <div id="types"></div>
+                        <button id="addTeam"></button>
+                    </div>`;
+                    content.insertAdjacentHTML("beforeend", pokList);
+                });
+            });
     }
 };
+
+pokemonlist.init();
