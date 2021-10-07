019b0f2d6a35a38eeecb5dd484d18bc24603483b
Freya-c23 <freya.claes@student.ehb.be>
2021-10-07 14:37:44 +0200

    starting from previous commit

--- Session 2/js/script.js
+++ Session 2/js/script.js
@@ -2,7 +2,6 @@
 
 import Team from "./team.js";
 
-let pokemonArray = [];
 let team = new Team();
 
 function getData() {
@@ -18,16 +17,15 @@ function getData() {
                 fetch(poke.url)
                     .then(response => response.json())
                     .then(data2 => {
-                        pokemonArray.push(data2);
-                        // const pokList = `
-                        // <div class="pokemon">
-                        //     <img src="${data2.sprites.front_default}" alt="">
-                        //     <p>nr.${data2.id}</p>
-                        //     <h1 id="name">${poke.name}</h1>
-                        //     <div id="types">${data2.types}</div>
-                        //     <button id="${data2.id}">add to team</button>
-                        // </div>`;
-                        // content.insertAdjacentHTML("beforeend", pokList);
+                        const pokList = `
+                        <div class="pokemon">
+                            <img src="${data2.sprites.front_default}" alt="">
+                            <p>nr.${data2.id}</p>
+                            <h1 id="name">${poke.name}</h1>
+                            <div id="types">${data2.types}</div>
+                            <button id="${data2.id}">add to team</button>
+                        </div>`;
+                        content.insertAdjacentHTML("beforeend", pokList);
                     });
             });
         });
