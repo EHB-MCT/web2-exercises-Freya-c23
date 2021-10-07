82ce3002aa89fad215c758c0a296879ef8a58e56
Freya-c23 <freya.claes@student.ehb.be>
2021-10-07 11:39:12 +0200

    changed according to feedback

--- Session 2/js/index.js
+++ Session 2/js/index.js
@@ -1,24 +1,29 @@
 'use strict';
 
-let pokemonlist = {
-    init() {
-        const content = document.getElementById("pokemonList");
-        content.innerHTML = " ";
-        fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`, {})
-            .then(response => response.json())
-            .then(data => {
-                data.results.forEach(function (pokemon) {
-                    const pokList = `
-                    <div class="pokemon">
-                        <img src="https://picsum.photos/200" alt="">
-                        <h1 id="name">${pokemon.name}</h1>
-                        <div id="types"></div>
-                        <button id="addTeam"></button>
-                    </div>`;
-                    content.insertAdjacentHTML("beforeend", pokList);
-                });
-            });
-    }
-};
+import team from "./team.js";
 
-pokemonlist.init();
+function getData() {
+    const content = document.getElementById("pokemonList");
+    content.innerHTML = " ";
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=151`)
+        .then(response => response.json())
+        .then(data => {
+            data.results.forEach(function (poke) {
+                fetch(poke.url)
+                    .then(response => response.json())
+                    .then(data2 => {
+                        const pokList = `
+                        <div class="pokemon">
+                            <img src="${data2.sprites.front_default}" alt="">
+                            <p>nr.${data2.id}</p>
+                            <h1 id="name">${poke.name}</h1>
+                            <div id="types">${data2.types}</div>
+                            <button id="${data2.id}">add to team</button>
+                        </div>`;
+                        content.insertAdjacentHTML("beforeend", pokList);
+                    });
+            });
+        });
+}
+
+getData();
