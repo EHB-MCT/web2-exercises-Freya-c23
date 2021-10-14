181bc62e1bcf397856714b1a9617403c7ae700b9
Freya-c23 <freya.claes@student.ehb.be>
2021-10-14 10:24:52 +0200

    promises added, button doesnt work yet

--- Session 3/js/script.js
+++ Session 3/js/script.js
@@ -2,12 +2,21 @@
 
 function randomNumber() {
     let compNumber = Math.floor(Math.random() * (21 - 1) + 1);
-
-    let promise = new Promise(function (resolve, reject) {
-
+    let inputNumber = document.getElementById("button").addEventListener('submit', e => {
+        e.preventDefault();
+        let number = document.getElementById('numb').value;
+        console.log(number);
     });
 
-    promise.then();
+    let promise = new Promise(function (resolve, reject) {
+        resolve(compNumber);
+        reject(isNaN(inputNumber));
+    });
+
+    promise.then(
+        result => console.log(result, "You have guessed the mystery number!"),
+        error => console.log(error)
+    );
 }
 
 randomNumber();
