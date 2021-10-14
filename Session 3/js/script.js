ee9c0e75734cd27686e9fc8fe05cb77d0c8c7270
Freya-c23 <freya.claes@student.ehb.be>
2021-10-14 09:52:58 +0200

    random number added

---
+++ Session 3/js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function randomNumber() {
+    let compNumber = Math.floor(Math.random() * (21 - 1) + 1);
+
+    let promise = new Promise(function (resolve, reject) {
+
+    });
+
+    promise.then();
+}
+
+randomNumber();
